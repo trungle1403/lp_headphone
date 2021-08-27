ce9ec14367018137b06a2c860f84264e81a83fd3
trungle1403 <64635586+trungle1403@users.noreply.github.com>
2021-08-27 17:13:29 +0700

    add button animation and js

--- js/main.js
+++ js/main.js
@@ -1,92 +1,9 @@
-// /*=============== SHOW MENU ===============*/
-// const navMenu = document.getElementById('nav-menu'),
-//       navToggle = document.getElementById('nav-toggle'),
-//       navClose = document.getElementById('nav-close')
-
-// /*===== MENU SHOW =====*/
-// /* Validate if constant exists */
-// if(navToggle){
-//     navToggle.addEventListener('click', () =>{
-//         navMenu.classList.add('show-menu')
-//     })
-// }
-
-// /*===== MENU HIDDEN =====*/
-// /* Validate if constant exists */
-// if(navClose){
-//     navClose.addEventListener('click', () =>{
-//         navMenu.classList.remove('show-menu')
-//     })
-// }
-
-// /*=============== REMOVE MENU MOBILE ===============*/
-// const navLink = document.querySelectorAll('.nav__link')
-
-// function linkAction(){
-//     const navMenu = document.getElementById('nav-menu')
-//     // When we click on each nav__link, we remove the show-menu class
-//     navMenu.classList.remove('show-menu')
-// }
-// navLink.forEach(n => n.addEventListener('click', linkAction))
-
-// /*=============== CHANGE BACKGROUND HEADER ===============*/
-// function scrollHeader(){
-//     const header = document.getElementById('header')
-//     // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-//     if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
-// }
-// window.addEventListener('scroll', scrollHeader)
-
-// /*=============== SHOW SCROLL UP ===============*/ 
-// function scrollUp(){
-//     const scrollUp = document.getElementById('scroll-up');
-//     // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
-//     if(this.scrollY >= 200) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
-// }
-// window.addEventListener('scroll', scrollUp)
-
-// /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-// const sections = document.querySelectorAll('section[id]')
-
-// function scrollActive(){
-//     const scrollY = window.pageYOffset
-
-//     sections.forEach(current =>{
-//         const sectionHeight = current.offsetHeight
-//         const sectionTop = current.offsetTop - 50;
-//         sectionId = current.getAttribute('id')
-
-//         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-//             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-//         }else{
-//             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
-//         }
-//     })
-// }
-// window.addEventListener('scroll', scrollActive)
-
-// /*=============== SCROLL REVEAL ANIMATION ===============*/
-// const sr = ScrollReveal({
-//     distance: '60px',
-//     duration: 2500,
-//     delay: 400,
-//     // reset: true
-// })
-
-// sr.reveal(`.home__header, .section__title`,{delay: 600})
-// sr.reveal(`.home__footer`,{delay: 700})
-// sr.reveal(`.home__img`,{delay: 900, origin: 'top'})
-
-// sr.reveal(`.sponsor__img, .products__card, .footer__logo, .footer__content, .footer__copy`,{origin: 'top', interval: 100})
-// sr.reveal(`.specs__data, .discount__animate`,{origin: 'left', interval: 100})
-// sr.reveal(`.specs__img, .discount__img`,{origin: 'right'})
-// sr.reveal(`.case__img`,{origin: 'top'})
-// sr.reveal(`.case__data`)
 
 const navMenu = document.querySelector('.nav-list')
 const header = document.querySelector('.header');
 const btnClose = document.querySelector('.nav-close')
 const btnOpen = document.querySelector('.nav-toggle')
+const btnToTop = document.querySelector('.btn-to-top')
 
 btnOpen.addEventListener('click', () => {
     navMenu.classList.add('open')
@@ -99,4 +16,54 @@ btnClose.addEventListener('click', () => {
 
 window.addEventListener('scroll',() =>{
     header.classList.toggle('sticky',window.scrollY > 50);
+    btnToTop.classList.toggle('active',window.scrollY > 200);
 })
+
+btnToTop.addEventListener('click',() =>{
+    document.documentElement.scrollTop = 0
+})
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav-link')
+
+navLink.forEach(n => n.addEventListener('click', ()=>{
+    navMenu.classList.remove('open')
+}))
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActiveLink() {
+    const scrollY = window.pageYOffset
+    sections.forEach( s => {
+        //height of section
+        const sectionHeight = s.offsetHeight
+        //sum height from top to section 0 620 1230 2000 ....
+        //70 is height header
+        const sectionTop = s.offsetTop - 90;
+        sectionId = s.getAttribute('id')
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav-list a[href*='+ sectionId+']').classList.add('active')
+        }else {
+            document.querySelector('.nav-list a[href*='+ sectionId+']').classList.remove('active')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActiveLink)
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true
+})
+
+sr.reveal(`.nav-logo, .nav-list, .section-title`,{delay: 400})
+sr.reveal(`.home-content`,{delay: 500})
+sr.reveal(`.home-img`,{delay: 700, origin: 'top'})
+sr.reveal(`.brand-img, .product-item, .footer-logo, .footer-column, .footer-copyright`,{origin: 'top', interval: 100})
+sr.reveal(`.case-img`,{origin: 'top'})
+sr.reveal(`.case-content`)
+sr.reveal(`.specs-item`,{origin: 'left', interval: 100})
+sr.reveal(`.specs-img, .discount-img`,{origin: 'right'})
+sr.reveal(`.discount-content`,{origin: 'left'})
